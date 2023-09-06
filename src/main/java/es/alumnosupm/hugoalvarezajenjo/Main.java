bf30275c376f4d9b073607130a182b4a0cf865c2
Hugo Alvarez Ajenjo <hugo.alvarezajenjo@gmail.com>
2023-09-06 12:24:13 +0200

    feat: Provisional Main Method

--- src/main/java/es/alumnosupm/hugoalvarezajenjo/Main.java
+++ src/main/java/es/alumnosupm/hugoalvarezajenjo/Main.java
@@ -1,7 +1,11 @@
 package es.alumnosupm.hugoalvarezajenjo;
 
+import es.alumnosupm.hugoalvarezajenjo.model.Board;
+import es.alumnosupm.hugoalvarezajenjo.view.BoardListerner;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Minesweeper game");
+        new BoardListerner(new Board());
     }
 }
