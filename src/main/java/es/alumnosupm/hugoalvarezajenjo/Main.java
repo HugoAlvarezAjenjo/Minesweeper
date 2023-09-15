87006f73572f844437ecf065d5564903e1f246fd
Hugo Alvarez Ajenjo <hugo.alvarezajenjo@gmail.com>
2023-09-15 13:17:44 +0200

    chore: :bookmark: Release 1.0
    Remove unecesary code

--- src/main/java/es/alumnosupm/hugoalvarezajenjo/Main.java
+++ src/main/java/es/alumnosupm/hugoalvarezajenjo/Main.java
@@ -12,7 +12,6 @@ import es.alumnosupm.hugoalvarezajenjo.view.BoardListerner;
  */
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Minesweeper game");
         new BoardListerner(new Board());
     }
 }
